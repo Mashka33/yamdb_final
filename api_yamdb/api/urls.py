fee2b73f4fa39b70edadd968ef9c873457dbcb83
Мария Степанова <mstepanova188@gmail.com>
2023-02-18 11:50:22 +0300

    flake

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework import routers
 
 from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
-                    ReviewViewSet,  TitleViewSet)
+                    ReviewViewSet, TitleViewSet)
 
 app_name = 'api'
 

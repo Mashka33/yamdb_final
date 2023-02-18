18f17ae39f63b6f1ea5b567c3827789b26419255
Мария Степанова <mstepanova188@gmail.com>
2023-02-18 11:18:07 +0300

    flake

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,8 +1,8 @@
-from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+from users.models import User
 
 from .validators import year_validator
-from users.models import User
 
 LINE_SLICE = 20
 
